4f68f9fdf5f1cc593fe90a43e2073da83082c42c
loraxx753 <loraxx753@gmail.com>
2013-05-24 10:20:49 -0400

    Create localstorage.object.js

---
+++ src/localstorage.object.js
@@ -0,0 +1,73 @@
+/**
+ * The HTML5 localStorage feature only saves strings. So how do you save an
+ * object, with properties and methods, to localStorage so you can access it
+ * between page loads?
+ */
+
+/**
+ * Takes in an object and turns it into a string
+ *
+ * Taken from www.davidpirek.com/blog/object-to-string-how-to-deserialize-json‎
+ *
+ * @param  {object} The object to convert
+ * @return {string} The resulting object turned into a string
+ */
+var objectToString = function(object){
+  	
+		// Initialize the variables
+		var array = [], propertyValue;
+		
+		// Loop through the object's properties
+		for(var property in object){
+			
+			// Check if the object actually has that property
+			if(object.hasOwnProperty(property)){
+				
+				// Get the value of the object's property
+				propertyValue = object[property];
+
+				// If the property has a value, and that value is an object
+				if(propertyValue && typeof propertyValue == "object"){
+					console.log(arguments.callee(propertyValue).join(", "));
+					array[array.length]= '"'+property+'"' + ":{" + arguments.callee(propertyValue).join(", ") + "}";
+				}
+				else {
+
+					// If the property value is a string, then call .toString() on it and wrap it in double quotes.
+					if(typeof propertyValue == "string"){
+						array[array.length] = [ '"'+property+'"'+ ":\"" + propertyValue.toString() + "\"" ];                 
+					}
+					// If it's not a string or an object, then just straight up .toString it
+					else{
+						array[array.length] = [ '"'+property+'"'+ ":" + propertyValue.toString()];
+					}
+				}
+			}
+		}
+
+	// Return the parsed object's array joined with commas, and wrapped in curly braces.
+	return "{" + array.join(", ") + "}";
+};
+
+var updateObject = function(object)
+{
+	// If HTML5's localStorage is available (by checking the Storage object) 
+	if(typeof(Storage)!=="undefined")
+	{
+		// If the localStorage's variable doesn't exist yet.
+		if(!localStorage.savedObject)
+		{
+			// Set that variable to the desired object.
+			localStorage.savedObject = objectToString(object);
+		}
+		// But if it DOES exist.
+		else
+		{
+			// Since localStorage only saves strings, then add a return before self calling the function.
+			var objectHolder = "return "+localStorage.savedObject;
+
+			// Set the original variable to localStorage's updated information.
+			return (new Function(objectHolder))();
+		}
+	}
+};
